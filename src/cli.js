34077d7200cef5061f4eb10db5ac2bc2db30c300
recyclebin5385 <recyclebin5385@yahoo.co.jp>
2019-10-07 00:03:55 +0900

    Modify command line interface

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,38 @@
 const webScreenshot = require('./index')
 const program = require('commander')
+const yaml = require('js-yaml')
+const fs = require('fs')
 
 async function main () {
   program
     .version('@@pkg.version')
     .usage('[options] URLs...')
-    .option('-w, --width <number>', 'Width of the screenshot', (value, previous) => parseInt(value), 1024)
-    .option('-h, --height <number>', 'Height of the screenshot, 0 for unlimited', (value, previous) => parseInt(value), 0)
-    .option('-o, --out <pattern>', 'Pattern of the output image file path', value => value, 'image{{count}}.png')
-    // .option('-c, --config <file>', 'Configuration file path')
+    .option('-w, --width <number>', 'Width of the screenshot', (value, previous) => parseInt(value))
+    .option('-h, --height <number>', 'Height of the screenshot, 0 for unlimited', (value, previous) => parseInt(value))
+    .option('-o, --out <pattern>', 'Pattern of the output image file path', value => value)
+    .option('-c, --config <file>', 'Configuration file path')
     .helpOption('--help')
   program.parse(process.argv)
 
-  const renderer = webScreenshot.newRenderer({
-    render: {
-      width: program.width,
-      height: program.height,
-      outputPath: program.out
-    }
-  })
+  let options = {}
+  if (program.config) {
+    options = yaml.safeLoad(fs.readFileSync(program.config, 'utf8'))
+  }
+  if (!options.render) {
+    options.render = {}
+  }
+
+  if (program.width != null) {
+    options.render.width = program.width
+  }
+  if (program.height != null) {
+    options.render.height = program.height
+  }
+  if (program.out != null) {
+    options.render.outputPath = program.out
+  }
+
+  const renderer = webScreenshot.newRenderer(options)
 
   try {
     for (const url of program.args) {
