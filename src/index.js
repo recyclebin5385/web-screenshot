5fefc19a6a08b40cde525f3c68ec9249634609a8
recyclebin5385 <recyclebin5385@yahoo.co.jp>
2019-10-07 00:01:30 +0900

    Organize JSDoc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /*!
- * @@pkg.name <@@pkg.homepage>
+ * @@pkg.name
  *
  * @@pkg.copyright
  *
@@ -12,21 +12,45 @@ const mkdirp = require('mkdirp')
 const Handlebars = require('handlebars')
 
 /**
- * Description of the module.
+ * The module for creating a web page screenshot.
  *
  * @module @@pkg.name
  */
 
+/**
+ * @classdesc A class which renders web pages as images.
+ * @class
+ * @hideconstructor
+ */
 function Renderer (options) {
   this.options = Object.assign({}, options)
   this.browserPromise = puppeteer.launch(this.options.puppeteer)
   this.renderCount = 0
 }
 
-Renderer.create = function (options) {
-  return new Renderer(options)
-}
-
+/**
+ * Render the specified web page and save the image.
+ *
+ * A {@link https://handlebarsjs.com/ Handlebars} template can be used in "options.outputPath".
+ * Below is the variables available in the template.
+ *
+ * <dl>
+ * <dt>count</dt>
+ * <dd>The number of URLs which have been rendered</dd>
+ * <dt>url</dt>
+ * <dd>The URL</dd>
+ * <dt>urlBasename</dt>
+ * <dd>The base name of the URL</dd>
+ * </dl>
+ *
+ * @async
+ * @param {string} url The URL
+ * @param {Object} [options] The options
+ * @param {Number} [options.width] The width of the image
+ * @param {Number} [options.height] The height of the image, 0 for unlimited
+ * @param {string} [options.outputPath] The file path of the saved image
+ * @returns {Promise} The promise object
+ */
 Renderer.prototype.render = async function (url, options) {
   options = Object.assign({
     width: 1024,
@@ -56,19 +80,27 @@ Renderer.prototype.render = async function (url, options) {
   })
 }
 
+/**
+ * Dispose the renderer.
+ *
+ * @async
+ * @returns {Promise} The promise object
+ */
 Renderer.prototype.dispose = async function () {
   const browser = await this.browserPromise
   await browser.close()
 }
 
 /**
- * Do something.
+ * Create a new renderer.
  *
- * @param {Object} options Options
- * @param {string} options.foo FooBar
+ * For details of "options.puppeteer", see {@link https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions}.
+ *
+ * @param {Object} [options] The options
+ * @param {Object} [options.puppeteer] The options passed to Puppeteer
+ * @param {Object} [options.render] The default options of {@link module:@@pkg.name~Renderer#render}
+ * @returns {@link module:@@pkg.name~Renderer} The renderer
  */
-module.exports = {
-  newRenderer: function (options) {
-    return new Renderer(options)
-  }
+exports.newRenderer = function (options) {
+  return new Renderer(options)
 }
